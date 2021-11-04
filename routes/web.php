6744904b8e560078e48e551a5d8d9c2f1da766cf
Guilherme M. Pires <guilherme.pires@unicred.com.br>
2021-11-04 07:34:11 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/series', function(){
+    $series = [
+        'GOT',
+        'TWD',
+        'FTWD'
+    ];
+    $html = "<ul>";
+    foreach ($series as $serie){
+        $html .= "<li>$serie</li>";
+    }
+    $html .= "</ul>";
+   return $html;
+});
